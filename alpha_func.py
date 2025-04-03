55a7dbab82699f7ae651947fb7a21564bb5bf983
MarkusBraun <narkusbraun17@gmail.com>
2025-04-03 17:01:08 +0200

    tried to increase the speed as much as possible (yet)

--- alpha_func.py
+++ alpha_func.py
@@ -1,33 +1,31 @@
 from sigma_cdm_func import *
 import numpy as np
 import scipy.interpolate as intp
+from numba import njit, prange
 
 m_res=1e8
 m_max=1e15
 
-lsig = lambda M: np.log(sigma(M))
+@njit(parallel=True, fastmath=True)
+def compute_log_sig(m_array):
+    log_sig = np.zeros_like(m_array)
+    for i in prange(len(m_array)):
+        log_sig[i] = np.log(sigma_cdm(m_array[i]))
+    return log_sig
 
-m_array = np.linspace(m_res,m_max,100)
 
-logSig = np.zeros_like(m_array)
-for i in range(len(m_array)):
-    logSig[i] = lsig(m_array[i])
+# lsig = lambda M: np.log(sigma(M))
+
+m_array = np.geomspace(1e8,1e15,200)
+
+logSig =  compute_log_sig(m_array) #np.zeros_like(m_array)
+# for i in range(len(m_array)):
+#     logSig[i] = lsig(m_array[i])
 
 logM = np.log(m_array)
 
-interpolation = intp.UnivariateSpline(logM,logSig,k=4)
+interpolation = intp.UnivariateSpline(logM,logSig,k=4,s=.1)
+alpha_ret = interpolation.derivative(n=1)
 
 def alpha(m):
-    # lsig = lambda M: np.log(sigma_cdm(M,a))
-
-    # m_array = np.linspace(m_res,m_max)
-
-    # logSig = []
-    # for i in range(len(m_array)):
-    #     logSig.append(lsig(m_array[i]))
-
-    # logM = np.log(m_array)
-
-    # interpolation = intp.UnivariateSpline(logM,logSig,k=4)
-    alpha_ret = interpolation.derivative(n=1)
     return alpha_ret(np.log(m))
