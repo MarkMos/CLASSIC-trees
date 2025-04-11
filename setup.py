763218f6f48395a99dd0804d67d44a169c4ad61b
MarkusBraunPhys <markusbraun17@gmail.com>
2025-04-11 13:40:31 +0200

    setup.py added to C_version branch

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension(
+        'classic_trees',
+        sources=['classic_trees.pyx'],
+        language='c',
+        extra_compile_args=['-std=c99'],
+        include_dirs=[np.get_include()]
+    )
+]
+
+setup(
+    name='classic_trees_module',
+    version='0.1',
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'language_level': '3',
+            'boundscheck': True,
+            'wraparound': True
+        }
+    ),
+)
