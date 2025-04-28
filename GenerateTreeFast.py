4cbcb465d131e65ee3caa94b2a4faa20e8776ca6
MarkusBraun <narkusbraun17@gmail.com>
2025-04-28 14:44:37 +0200

    removed unused lines and added an if-statement to make larger empty merger trees for larger masses

--- GenerateTreeFast.py
+++ GenerateTreeFast.py
@@ -18,69 +18,12 @@ filename = './CLASSIC-trees/Data/flat.txt'
 DELTA = functions(filename)
 
 def tree_process(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo):
-    # iter = 1
-    # i_err= 1 #Initialising error
-
-    # while i_err != 0 or iter == 1:
-    #     if iter == 1:
-    #         i_seed_0 -= 19*(1+i)
-    #     i_seed = i_seed_0
-    #     random.seed(i_seed)
-    #     print('Making a tree...',i)
-
-    #     # Make a merger-tree using the make_tree function
-    #     my_tree = make_tree(mp_halo,a_halo,m_res,a_lev,n_lev,n_halo_max,n_halo)
-    #     i_err = my_tree[0]
-    #     merger_tree = my_tree[-2]
-    #     iter +=1
-    # print('Made a tree',i)
+    if mp_halo[i] > 6e14:
+        n_halo_max=10000000
     count,arr_mhalo,arr_nodid,arr_treeid,arr_time,arr_1prog,arr_desc = get_tree_vals(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo)
-    # Initialising arrays to save important quantaties of the merger-tree and
-    # walking through tree to count the number of nodes using walk_tree function
-    # this_node = merger_tree[0]
-    # count = 0
-    # arr_mhalo = np.zeros(n_halo_max)-1              # array for halo masses
-    # arr_nodid = np.zeros(n_halo_max,dtype='int_')-1 # array for the node id's
-    # arr_treeid= np.zeros(n_halo_max,dtype='int_')-1 # array for the tree id's
-    # arr_time  = np.zeros(n_halo_max,dtype='int_')-1 # array for the scale factor of the nodes
-    # arr_1prog = np.zeros(n_halo_max,dtype='int_')-1 # array for first progenitors of nodes
-    # arr_desc  = np.zeros(n_halo_max,dtype='int_')-1 # array for the descandant of nodes
-    # while this_node is not None:
-    #     node_ID = count
-    #     arr_nodid[node_ID] = node_ID
-    #     arr_mhalo[node_ID] = this_node.mhalo
-    #     arr_treeid[node_ID]= i
-    #     arr_time[node_ID]  = this_node.jlevel
-    #     if this_node.child is not None:
-    #         arr_1prog[node_ID] = merger_tree.index(this_node.child)
-    #     else:
-    #         arr_1prog[node_ID] = -1
-    #     if this_node.parent is not None:
-    #         arr_desc[node_ID] = merger_tree.index(this_node.parent)
-    #     else:
-    #         arr_desc[node_ID] = -1
-    #     count +=1
-    #     this_node = walk_tree(this_node)
-
-    # # Trim arrays to actual size
-    # arr_mhalo = arr_mhalo[0:count]
-    # arr_nodid = arr_nodid[0:count]
-    # arr_treeid= arr_treeid[0:count]
-    # arr_time  = arr_time[0:count]
-    # arr_1prog = arr_1prog[0:count]
-    # arr_desc  = arr_desc[0:count]
     count = np.array([count],dtype='int_')
     i = np.array([i],dtype='int_')
 
-    # print('Number of nodes in tree',i+1,'is',count)
-    
-    # print('Example information from tree:')
-    # this_node = merger_tree[0]
-    # print('Base node: \n  mass =',this_node.mhalo,' z= ',1/a_lev[this_node.jlevel]-1,' number of progenitors ',this_node.nchild)
-    # this_node = this_node.child
-    # print('First progenitor: \n  mass =',this_node.mhalo,' z= ',1/a_lev[this_node.jlevel]-1)
-    # this_node = this_node.sibling
-    # print('  mass =',this_node.mhalo,' z= ',1/a_lev[this_node.jlevel]-1)
     return {
         "arr_mhalo": arr_mhalo,
         "arr_nodid": arr_nodid,
@@ -109,34 +52,34 @@ def parallel_exe(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo
     with Pool() as pool:
         results = pool.starmap(tree_process, args_list)
     print('Here')
-    # with h5py.File('./Code_own/Trees/tree_selftestfast_random_masses3.hdf5','a',libver='latest') as f:
-    #     # Create or access groups of the merger tree file
-    #     if 'TreeHalos' not in f:
-    #         grp1 = f.create_group('TreeHalos')
-    #     else:
-    #         grp1 = f['TreeHalos']
-    #         nth_run = True
-    #     if 'TreeTable' not in f:
-    #         grp2 = f.create_group('TreeTable')
-    #     else:
-    #         grp2 = f['TreeTable']
+    with h5py.File('./Code_own/Trees/tree_selftestfast_random_masses3.hdf5','a',libver='latest') as f:
+        # Create or access groups of the merger tree file
+        if 'TreeHalos' not in f:
+            grp1 = f.create_group('TreeHalos')
+        else:
+            grp1 = f['TreeHalos']
+            nth_run = True
+        if 'TreeTable' not in f:
+            grp2 = f.create_group('TreeTable')
+        else:
+            grp2 = f['TreeTable']
         
-    #     if nth_run is False:
-    #         grp3 = f.create_group('TreeTimes')
-    #         d_red = grp3.create_dataset('Redshift',data=1/a_lev-1)
-    #         d_time= grp3.create_dataset('Time',data=a_lev)
-    #     for result in results:
-    #         append_create_dataset(grp1,'SnapNum',result['arr_time'])
-    #         append_create_dataset(grp1,'SubhaloMass',data=result['arr_mhalo'])
-    #         append_create_dataset(grp1,'TreeDescendant',result['arr_desc'])
-    #         append_create_dataset(grp1,'TreeFirstProgenitor',result['arr_1prog'])
-    #         append_create_dataset(grp1,'TreeID',data=result['arr_treeid'])
-    #         append_create_dataset(grp1,'TreeIndex',data=result['arr_nodid'])
-    #         append_create_dataset(grp2,'Length',data=result['count'])
-    #         append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
-    #         append_create_dataset(grp2,'TreeID',data=result['tree_index'])
-    #         start_offset += result['count']
-    #     return start_offset
+        if nth_run is False:
+            grp3 = f.create_group('TreeTimes')
+            d_red = grp3.create_dataset('Redshift',data=1/a_lev-1)
+            d_time= grp3.create_dataset('Time',data=a_lev)
+        for result in results:
+            append_create_dataset(grp1,'SnapNum',result['arr_time'])
+            append_create_dataset(grp1,'SubhaloMass',data=result['arr_mhalo'])
+            append_create_dataset(grp1,'TreeDescendant',result['arr_desc'])
+            append_create_dataset(grp1,'TreeFirstProgenitor',result['arr_1prog'])
+            append_create_dataset(grp1,'TreeID',data=result['arr_treeid'])
+            append_create_dataset(grp1,'TreeIndex',data=result['arr_nodid'])
+            append_create_dataset(grp2,'Length',data=result['count'])
+            append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
+            append_create_dataset(grp2,'TreeID',data=result['tree_index'])
+            start_offset += result['count']
+        return start_offset
 
 if __name__ == '__main__':
     n_tree = 30
