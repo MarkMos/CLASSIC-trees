3ef6eeda1617896280cb1b0b8f9214e9cc526259
MarkusBraun <narkusbraun17@gmail.com>
2025-04-29 17:44:20 +0200

    new file with the velocity dispersion

---
+++ halo_velo_func.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy.integrate as itg
+from sigma_cdm_func import rho_crit
+import matplotlib.pyplot as plt
+
+filename = './CLASSIC-trees/pk_CLASS.txt'
+pk_data = np.loadtxt(filename)
+k_0  = pk_data[0]
+Pk_0 = pk_data[1]*0.73**3
+
+def sig_j_func(m,j,k=k_0,Pk=Pk_0):
+    R = (3*m/(4*np.pi*rho_crit))**(1/3)
+    my_integrand = k**(2+2*j)*9*(k*R*np.cos(k*R) - np.sin(k*R))**2 * Pk / k**6 / R**6 / 2 / np.pi**2
+    sig_j = np.sqrt(itg.simpson(my_integrand,k))
+    return sig_j
+
+def sig_halo_func(m,omega_0=0.25,H0=73):
+    return H0*omega_0**(0.6)*sig_j_func(m,-1)*np.sqrt(1-sig_j_func(m,0)**4/(sig_j_func(m,1)**2*sig_j_func(m,-1)**2))
+n = 100
+masses   = np.logspace(11,15,n)
+sig_halo = np.zeros(n)
+
+for i in range(n):
+    sig_halo[i] = sig_halo_func(masses[i])
+
+plt.plot(masses,sig_halo)
+plt.grid()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('m')
+plt.ylabel(r'$\sigma$')
+plt.savefig('sigma_halo.png')
