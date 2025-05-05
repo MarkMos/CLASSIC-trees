d36a01faf9c4838f7123cf338c2d7b3ddc17f410
MarkusBraun <narkusbraun17@gmail.com>
2025-05-05 17:35:42 +0200

    tested is it still worked

--- Generate_Tree.py
+++ Generate_Tree.py
@@ -24,28 +24,16 @@ def append_create_dataset(grp,name,data):
         grp.create_dataset(name,data=data,maxshape=(None,)+data.shape[1:])
 
 
-n_lev = 20
+n_lev = 10
 n_halo_lev = 10
 
-mp_halo = 1e10
+mp_halo = 1e11
 m_res   = 1e8
 n_tree  = 1
 
 # u_PS = np.random.rand(n_tree)
 # m_halos = ppf_PS(u_PS)
 # print(type(m_halos))
-
-G_0=0.57
-gamma_1=0.38
-gamma_2=-0.01
-eps_1=0.1
-eps_2=0.1
-
-omega_0=0.25
-lambda_0=0.75
-h_0=0.73
-omega_b=0.04
-Gamma=omega_0*h_0
 
 n_spec = 1
 dn_dlnk = 0
@@ -78,47 +66,47 @@ start_offset = 0
 start = time.time()
 for i in range(n_tree):
     count,arr_mhalo,arr_nodid,arr_treeid,arr_time,arr_1prog,arr_desc,arr_nextprog = get_tree_vals(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo)
-    with h5py.File('./Code_own/Trees/tree_selftestfast_1e10_ytree1.h5','a') as f:
-        # Create or access groups of the merger tree file
-        if 'TreeHalos' not in f:
-            grp1 = f.create_group('TreeHalos')
-        else:
-            grp1 = f['TreeHalos']
-            nth_run = True
-        if 'TreeTable' not in f:
-            grp2 = f.create_group('TreeTable')
-        else:
-            grp2 = f['TreeTable']
+    # with h5py.File('./Code_own/Trees/tree_selftestfast_1e10_ytree1.h5','a') as f:
+    #     # Create or access groups of the merger tree file
+    #     if 'TreeHalos' not in f:
+    #         grp1 = f.create_group('TreeHalos')
+    #     else:
+    #         grp1 = f['TreeHalos']
+    #         nth_run = True
+    #     if 'TreeTable' not in f:
+    #         grp2 = f.create_group('TreeTable')
+    #     else:
+    #         grp2 = f['TreeTable']
         
-        if nth_run is False:
-            grp3 = f.create_group('TreeTimes')
-            d_red = grp3.create_dataset('Redshift',data=1/a_lev-1)
-            d_time= grp3.create_dataset('Time',data=a_lev)
+    #     if nth_run is False:
+    #         grp3 = f.create_group('TreeTimes')
+    #         d_red = grp3.create_dataset('Redshift',data=1/a_lev-1)
+    #         d_time= grp3.create_dataset('Time',data=a_lev)
         
-        append_create_dataset(grp1,'SnapNum',arr_time)
-        append_create_dataset(grp1,'Mass',data=arr_mhalo)
-        append_create_dataset(grp1,'Descendant',arr_desc)
-        append_create_dataset(grp1,'FirstProgenitor',arr_1prog)
-        append_create_dataset(grp1,'NextProgenitor',arr_nextprog)
-        append_create_dataset(grp1,'TreeID',data=arr_treeid)
-        append_create_dataset(grp1,'TreeIndex',data=arr_nodid)
-        append_create_dataset(grp2,'Length',data=np.array([count]))
-        append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
-        append_create_dataset(grp2,'TreeID',data=np.array([i]))
-        grp = f.create_group('Header')
-        grp.attrs['LastSnapShotNr'] = 9
-        grp.attrs['Nhalos_ThisFile'] = 120000
-        grp.attrs['Nhalos_ThisFile'] = 1433267
-        grp.attrs['Nhalos_Total'] = 1433267
-        grp.attrs['Ntrees_ThisFile'] = 120000
-        grp.attrs['Ntrees_Total'] = 120000
-        grp.attrs['NumFiles'] = 1
-        if 'Parameters' not in f:
-            f.create_group('Parameters')
-            f['Parameters'].attrs['HubbleParam'] = 0.73
-            f['Parameters'].attrs['Omega0'] = 0.25
-            f['Parameters'].attrs['OmegaLambda'] = 0.75
-            f['Parameters'].attrs['BoxSize'] = 479.0
+    #     append_create_dataset(grp1,'SnapNum',arr_time)
+    #     append_create_dataset(grp1,'Mass',data=arr_mhalo)
+    #     append_create_dataset(grp1,'Descendant',arr_desc)
+    #     append_create_dataset(grp1,'FirstProgenitor',arr_1prog)
+    #     append_create_dataset(grp1,'NextProgenitor',arr_nextprog)
+    #     append_create_dataset(grp1,'TreeID',data=arr_treeid)
+    #     append_create_dataset(grp1,'TreeIndex',data=arr_nodid)
+    #     append_create_dataset(grp2,'Length',data=np.array([count]))
+    #     append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
+    #     append_create_dataset(grp2,'TreeID',data=np.array([i]))
+    #     grp = f.create_group('Header')
+    #     grp.attrs['LastSnapShotNr'] = 9
+    #     grp.attrs['Nhalos_ThisFile'] = 120000
+    #     grp.attrs['Nhalos_ThisFile'] = 1433267
+    #     grp.attrs['Nhalos_Total'] = 1433267
+    #     grp.attrs['Ntrees_ThisFile'] = 120000
+    #     grp.attrs['Ntrees_Total'] = 120000
+    #     grp.attrs['NumFiles'] = 1
+    #     if 'Parameters' not in f:
+    #         f.create_group('Parameters')
+    #         f['Parameters'].attrs['HubbleParam'] = 0.73
+    #         f['Parameters'].attrs['Omega0'] = 0.25
+    #         f['Parameters'].attrs['OmegaLambda'] = 0.75
+    #         f['Parameters'].attrs['BoxSize'] = 479.0
     # with h5py.File('ytree_compatible.h5', 'w') as f:
     #     # Required groups
     #     tree = f.create_group('Tree0')
