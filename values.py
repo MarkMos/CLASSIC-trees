df83964f868790c31d83e3aab70c8c4a91c49f71
MarkusBraun <narkusbraun17@gmail.com>
2025-05-06 18:56:12 +0200

    adjusted for two default cases and user self implementation using class

--- values.py
+++ values.py
@@ -1,9 +1,18 @@
+import numpy as np
 from classy import Class
 
+'''
+File to set cosmological parameters initially and add different cases.
+
+For adding a file:
+    - filestyle needs to be data[0] = k-values and data[1] = values of the 
+    Powerspectrum (not in h-units!!!)
+'''
+
 # file_name_pk = './CLASSIC-trees/pk_CLASS_default.txt'
-file_name_pk = './CLASSIC-trees/pk_CLASS_h_73.txt'
+# file_name_pk = './CLASSIC-trees/pk_CLASS_h_73.txt'
 # file_name_pk = './CLASSIC-trees/pk_CLASS.txt'
-# file_name_pk = None
+file_name_pk = None
 
 if file_name_pk=='./CLASSIC-trees/pk_CLASS_default.txt':
     cosmo = Class()
@@ -13,12 +22,19 @@ if file_name_pk=='./CLASSIC-trees/pk_CLASS_default.txt':
     l_0=1-omega_0
     h_0=cosmo.h()
 elif file_name_pk==None:
+    h_0 = 0.73
+    omega_0 = 0.25
+    l_0 = 0.75
+    z = np.array([0],dtype='float64')
+    N_k = 1000
+    k_array = np.zeros((N_k, 1, 1),dtype='float64')
+    k_array[:,0,0] = np.logspace(-6,3,N_k)
+
     cosmo = Class()
-    cosmo.set({'output':'mPk','P_k_max_h/Mpc':10000})
+    cosmo.set({'output':'mPk','P_k_max_h/Mpc':10000,'h':h_0,'Omega_m':omega_0,'Omega_Lambda':l_0})
     cosmo.compute()
-    omega_0=cosmo.Omega_m()
-    l_0=1-omega_0
-    h_0=cosmo.h()
+    Pk_0_np = cosmo.get_pk(k_array*h_0,z,N_k,1,1)[:,0,0]
+    k_0_np = k_array[:,0,0]
 else:
     omega_0=0.25
     l_0=0.75
