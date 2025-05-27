a134124aaf2de02f6233e39e3095bd213717ebcf
MarkusBraun <narkusbraun17@gmail.com>
2025-05-27 17:43:17 +0200

    minor changes for analysing

--- dist_halos.py
+++ dist_halos.py
@@ -6,20 +6,19 @@ from tqdm import tqdm
 f = h5py.File('tree_extended.hdf5')
 
 Snapnum = f['SnapNum'][:]
-times = [90] #np.arange(0,91,1)
+times = [90,89,88] #np.arange(0,91,1)
 TreeID = [0]
 
 
 pCM_abs  = []
-psub_abs = []
 
 # fig = plt.figure(figsize=(15,15))
 # ax = fig.add_subplot(projection='3d')
 # c_arr = ['b','r']
-for k in TreeID:
-    start = np.where(k==f['TreeID'][:])[0][0]
-    end   = np.where(k==f['TreeID'][:])[0][-1]
-    for n,number in enumerate(tqdm(times)):
+# for k in TreeID:
+#     start = np.where(k==f['TreeID'][:])[0][0]
+#     end   = np.where(k==f['TreeID'][:])[0][-1]
+#     for n,number in enumerate(times):
         # sub_p1 = []
         # sub_p2 = []
         # sub_p3 = []
@@ -39,44 +38,54 @@ for k in TreeID:
             #         sub_p1.append(f['SubhaloVel'][j][0]*sub_mass)
             #         sub_p2.append(f['SubhaloVel'][j][1]*sub_mass)
             #         sub_p3.append(f['SubhaloVel'][j][2]*sub_mass)
-        indx_CM = np.where(Snapnum==number)[0]
-        print(indx_CM)
-        mass = f['SubhaloMass'][indx_CM]
+        # indx_CM = np.where(Snapnum==number)[0]
+        # # print(indx_CM)
+        # mass = f['SubhaloMass'][indx_CM]*1e10
         # vel = f['GroupVel'][indx_CM]
-        print(f['FirstSubhaloInFOFGroupID'][:])
-        indx = [] 
-        for i in indx_CM:
-            ind = np.where(f['FirstSubhaloInFOFGroupID'][i]==f['FirstSubhaloInFOFGroupID'][:])[0][0]
-            if ind in indx:
-                continue
-            else:
-                indx.append(int(ind))
-        indx = np.sort(np.array(indx))
-        print(indx)
-        vel = f['GroupVel'][indx]
-        cen_mass = f['SubhaloMass'][indx]*1e10
+        # indx = [] 
+        # for i in tqdm(indx_CM):
+        #     ind = np.where(f['FirstSubhaloInFOFGroupID'][i]==f['FirstSubhaloInFOFGroupID'][:])[0][0]
+        #     if ind in indx:
+        #         continue
+        #     else:
+        #         indx.append(int(ind))
+        # indx = np.sort(np.array(indx))
+        # print(indx)
+        # vel = f['GroupVel'][indx]
+        # cen_mass = f['SubhaloMass'][indx]*1e10
         # indx_sub = np.where(f['DescendantID'][i+1]+1==f['DescendantID'][:length])[0]
         # sub_mass = f['SubhaloMass'][indx_CM]
-        sub_p = f['SubhaloVel'][indx_CM]
-        for i in range(len(vel)):
-            pCM_abs.append(sum(np.sqrt(vel**2)[i]))
-            psub_abs.append(sum(np.sqrt(sub_p**2)[i]))
+        # sub_p = f['SubhaloVel'][indx_CM]
+        # # vel_sq = vel**2
+        # psub_abs = []
+        # sub_p_sq = sub_p**2
+        # for i in tqdm(range(len(sub_p_sq))):
+        #     # pCM_abs.append(np.sqrt(sum(vel_sq[i])))
+        #     psub_abs.append(np.sqrt(sum(sub_p_sq[i])))
+mass = f['SubhaloMass'][:]*1e10
+sub_p = f['SubhaloVel'][:]
+psub_abs = []
+sub_p_sq = sub_p**2
+for i in tqdm(range(len(sub_p_sq))):
+    # pCM_abs.append(np.sqrt(sum(vel_sq[i])))
+    psub_abs.append(np.sqrt(sum(sub_p_sq[i])))
 # plt.hexbin(pCM_abs,psub_abs,cmap='inferno',bins='log',xscale='log',yscale='log')
 # plt.plot(Snapnum[:length],pCM_abs,label='Central',marker='.',lw=0)
 # plt.plot(Snapnum[:length],psub_abs,label='Sub',marker='.',lw=0)
 # plt.plot(Snapnum[:length],(np.array(pCM_abs)-np.array(psub_abs))/np.array(psub_abs),marker='.',lw=0)
-plt.hexbin(cen_mass,pCM_abs,cmap='inferno',bins='log',xscale='log',yscale='log')
+plt.hexbin(mass,psub_abs,cmap='inferno',bins='log',xscale='log',yscale='log')
 plt.colorbar()
 plt.grid()
 # plt.xlabel(r'$p_{cen}$')
-plt.xlabel(r'$M_{cen}$')
-plt.ylabel(r'$V_{cen}$')
+plt.xlabel(r'$M$')
+plt.ylabel(r'$\lambda$')
 # plt.xlabel('Snapshot')
 # plt.ylabel(r'$\Delta V$')
 # plt.legend()
 # plt.xscale('log')
 # plt.yscale('log')
-plt.savefig('Velocity_comparison.png')
+plt.savefig('SpinAll.png')
+# plt.show()
 
 # filenames = ['groups_088.hdf5']#,'groups_090.hdf5','groups_038.hdf5','groups_032.hdf5','groups_058.hdf5','groups_062.hdf5','groups_042.hdf5','groups_028.hdf5','groups_048.hdf5','groups_084.hdf5']
 # # sub_masses = []
