1394871bef6fee31a002499001582532af5ae02f
MarkusBraun <narkusbraun17@gmail.com>
2025-09-05 16:48:42 +0200

    added the FoF version for the multiprocessing and all needed remaining inputs for sage

--- GenerateTreeFast.py
+++ GenerateTreeFast.py
@@ -3,7 +3,7 @@
 # from Moving_in_Tree import *
 # from Delta_crit import *
 # from sigma_cdm_func import *
-from classic_trees import get_tree_vals, functions
+from classic_trees import get_tree_vals, get_tree_vals_FoF, functions
 # from random_masses import ppf_ST, ppf_PS
 import numpy as np
 import h5py
@@ -19,7 +19,7 @@ def tree_process(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_
     if mp_halo > 6e14:
         # Safety to ensure that the merger-tree can be calculated.
         n_halo_max=10000000
-    count,arr_mhalo,arr_Vmax,arr_nodid,arr_treeid,arr_time,arr_1prog,arr_desc,arr_nextprog,arr_pos,arr_velo,arr_spin = get_tree_vals(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo,pos_base,vel_base,scaling)
+    count,arr_mhalo,arr_Vmax,arr_nodid,arr_treeid,arr_time,arr_1prog,arr_desc,arr_nextprog,arr_pos,arr_velo,arr_spin,arr_sublen = get_tree_vals(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo,pos_base,vel_base,scaling)
     count = np.array([count],dtype='int_')
     i = np.array([i],dtype='int_')
 
@@ -35,6 +35,43 @@ def tree_process(i,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_
         'arr_pos': arr_pos,
         'arr_velo': arr_velo,
         'arr_spin': arr_spin,
+        'arr_sublen': arr_sublen,
+        'count': count,
+        'tree_index': i
+    }
+
+def tree_process_FoF(i,i_seed_0,mp_halo,a_halo,m_res,m_min,w_lev,a_lev,n_lev,n_halo_max,n_halo,pos_base,vel_base,scaling,Boxsize):
+    vel_base = np.random.lognormal(np.log(200),0.7,3)
+    pos_base = np.random.uniform(0,Boxsize,3)
+    if mp_halo > 6e14:
+        # Safety to ensure that the merger-tree can be calculated.
+        n_halo_max=10000000
+    count,arr_mhalo,arr_Vmax,arr_nodid,arr_treeid,arr_time,arr_1prog,arr_desc,arr_nextprog,arr_1FoF,arr_nextFoF,arr_pos,arr_velo,arr_spin,arr_GroupMass,arr_sublen = get_tree_vals_FoF(i,i_seed_0,mp_halo,a_halo,m_min,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo,pos_base,vel_base,scaling)
+    arr_MostBoundID = np.zeros(np.sum(count),dtype='int_')
+    arr_vel_disp = np.zeros(np.sum(count))
+    arr_SubhaloNr = np.zeros(np.sum(count),dtype='int_')
+    count = np.array(count,dtype='int_')
+    i = np.array([i],dtype='int_')
+
+    return {
+        'arr_mhalo': arr_mhalo/1e10,
+        'arr_Vmax': arr_Vmax,
+        'arr_nodid': arr_nodid,
+        'arr_treeid': arr_treeid,
+        'arr_time': arr_time,
+        'arr_1prog': arr_1prog,
+        'arr_desc': arr_desc,
+        'arr_nextprog': arr_nextprog,
+        'arr_1FoF': arr_1FoF,
+        'arr_nextFoF': arr_nextFoF,
+        'arr_pos': arr_pos,
+        'arr_velo': arr_velo,
+        'arr_spin': arr_spin,
+        'arr_GroupMass': arr_GroupMass/1e10,
+        'arr_sublen': arr_sublen,
+        'arr_MostBoundID': arr_MostBoundID,
+        'arr_vel_disp': arr_vel_disp,
+        'arr_SubhaloNr': arr_SubhaloNr,
         'count': count,
         'tree_index': i
     }
@@ -88,12 +125,66 @@ def parallel_exe(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo
             append_create_dataset(grp1,'SubhaloPos',result['arr_pos'])
             append_create_dataset(grp1,'SubhaloVelo',result['arr_velo'])
             append_create_dataset(grp1,'SubhaloSpin',result['arr_spin'])
+            append_create_dataset(grp1,'SubhaloLen',result['arr_sublen'])
             append_create_dataset(grp1,'TreeID',data=result['arr_treeid'])
             append_create_dataset(grp1,'TreeIndex',data=result['arr_nodid'])
             append_create_dataset(grp2,'Length',data=result['count'])
             append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
             append_create_dataset(grp2,'TreeID',data=result['tree_index'])
             start_offset += result['count']
+        return start_offset
+
+def parallel_exe_FoF(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,m_min,w_lev,a_lev,n_lev,n_halo_max,n_halo,nth_run,start_offset,file_name,omega_0,l_0,h_0,BoxSize,mode,pos_base,vel_base,scaling):
+    args_list = [(i,i_seed_0,mp_halo[i],a_halo,m_res,m_min,w_lev,a_lev,n_lev,n_halo_max,n_halo,pos_base,vel_base,scaling,BoxSize)
+                  for i in range(j*n_tree,n_tree+j*n_tree)]
+    with Pool() as pool:
+        results = pool.starmap(tree_process_FoF, args_list)
+    print('Here')
+    with h5py.File(file_name,'a',libver='latest') as f:
+        # Create or access groups of the merger tree file
+        if 'TreeHalos' not in f:
+            grp1 = f.create_group('TreeHalos')
+        else:
+            grp1 = f['TreeHalos']
+            nth_run = True
+        if 'TreeTable' not in f:
+            grp2 = f.create_group('TreeTable')
+        else:
+            grp2 = f['TreeTable']
+        
+        if nth_run is False:
+            grp3 = f.create_group('TreeTimes')
+            d_red = grp3.create_dataset('Redshift',data=1/a_lev-1)
+            d_time= grp3.create_dataset('Time',data=a_lev)
+        if 'Parameters' not in f:
+            f.create_group('Parameters')
+            f['Parameters'].attrs['HubbleParam'] = h_0 #0.6781
+            f['Parameters'].attrs['Omega0'] = omega_0 #0.30988304304812053
+            f['Parameters'].attrs['OmegaLambda'] = l_0 #0.6901169569518795
+            f['Parameters'].attrs['BoxSize'] = BoxSize
+        for result in results:
+            append_create_dataset(grp1,'SnapNum',result['arr_time'])
+            append_create_dataset(grp1,'SubhaloMass',data=result['arr_mhalo'])
+            append_create_dataset(grp1,'SubhaloVmax',result['arr_Vmax'])
+            append_create_dataset(grp1,'TreeDescendant',result['arr_desc'])
+            append_create_dataset(grp1,'TreeFirstProgenitor',result['arr_1prog'])
+            append_create_dataset(grp1,'TreeNextProgenitor',result['arr_nextprog'])
+            append_create_dataset(grp1,'TreeFirstHaloInFOFgroup',result['arr_1FoF'])
+            append_create_dataset(grp1,'TreeNextHaloInFOFgroup',result['arr_nextFoF'])
+            append_create_dataset(grp1,'Group_M_Crit200',result['arr_GroupMass'])
+            append_create_dataset(grp1,'SubhaloPos',result['arr_pos'])
+            append_create_dataset(grp1,'SubhaloVel',result['arr_velo'])
+            append_create_dataset(grp1,'SubhaloSpin',result['arr_spin'])
+            append_create_dataset(grp1,'SubhaloLen',result['arr_sublen'])
+            append_create_dataset(grp1,'SubhaloIDMostbound',result['arr_MostBoundID'])
+            append_create_dataset(grp1,'SubhaloVelDisp',result['arr_vel_disp'])
+            append_create_dataset(grp1,'SubhaloNr',result['arr_SubhaloNr'])
+            append_create_dataset(grp1,'TreeID',data=result['arr_treeid'])
+            append_create_dataset(grp1,'TreeIndex',data=result['arr_nodid'])
+            append_create_dataset(grp2,'Length',data=np.array([int(np.sum(result['count']))],dtype='int_'))
+            append_create_dataset(grp2,'StartOffset',data=np.array([start_offset]))
+            append_create_dataset(grp2,'TreeID',data=result['tree_index'])
+            start_offset += np.sum(result['count'])
         return start_offset
 
 def compute_tree_fast(random_mass,
@@ -107,6 +198,7 @@ def compute_tree_fast(random_mass,
                       i_seed_0 = -8635,
                       a_halo = 1,
                       m_res = 1e8,
+                      m_min = 1e11,
                       z_max  = 4,
                       n_lev = 10,
                       n_halo_max = 1000000,
@@ -206,8 +298,12 @@ def compute_tree_fast(random_mass,
     w_lev = np.array(w_lev)
     nth_run = False
     start_offset = 0
-    for j in range(n_part):
-        start_offset = parallel_exe(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo,nth_run,start_offset,file_name,omega_0, l_0, h_0,BoxSize,mode,pos_base,vel_base,scaling)
+    if mode!='FoF':
+        for j in range(n_part):
+            start_offset = parallel_exe(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,w_lev,a_lev,n_lev,n_halo_max,n_halo,nth_run,start_offset,file_name,omega_0, l_0, h_0,BoxSize,mode,pos_base,vel_base,scaling)
+    else:
+        for j in range(n_part):
+            start_offset = parallel_exe_FoF(j,n_tree,i_seed_0,mp_halo,a_halo,m_res,m_min,w_lev,a_lev,n_lev,n_halo_max,n_halo,nth_run,start_offset,file_name,omega_0, l_0, h_0,BoxSize,mode,pos_base,vel_base,scaling)
     with h5py.File(file_name,'a') as f:
         grp = f.create_group('Header')
         grp.attrs['LastSnapShotNr'] = int(n_lev - 1)
