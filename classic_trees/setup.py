87052db64232f2eeec38dd3b60fa63df7d41acc7
MarkusBraun <narkusbraun17@gmail.com>
2025-11-18 18:02:03 +0100

    made this there own package

---
+++ classic_trees/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+import numpy as np
+import sysconfig
+python_include = sysconfig.get_paths()['include']
+
+extensions = [
+    Extension(
+        'classic_trees',
+        sources=['src/classic_trees.pyx'],
+        language='c',
+        extra_compile_args=['-std=c99'],
+        include_dirs=[np.get_include(),python_include]
+    )
+]
+
+setup(
+    name='classic_trees',
+    version='0.0.1',
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'language_level': '3',
+            'boundscheck': False,
+            'wraparound': False
+        }
+    ),
+    # packages= ['classic_trees'],
+    # package_dir={'classic_trees':'src'},
+    # package_data={'classic_trees':['src/classic_trees.pyx']},
+    # install_requires=[
+    #     "numpy",  # for numpy headers
+    #     "astropy",
+    #     "scipy"
+    # ],
+    # include_package_data=True,
+    # zip_safe = False,
+)
